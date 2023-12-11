5f09d6597a7fec2c35c9a2c161615c95800608a1
Tuan Do Van <do.van.tuan@seznam.cz>
2023-12-11 16:38:23 +0100

    first commit

---
+++ src/main/java/com/example/todoz/TodozApplication.java
@@ -0,0 +1,13 @@
+package com.example.todoz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodozApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodozApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/todoz/TodozApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todoz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodozApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
