667494e449f06b7ef73ea34350b3ee885231482c
BelovUser <139882857+BelovUser@users.noreply.github.com>
2023-12-12 11:31:11 +0100

    Created filterByRating method that returns List<Task> by rating number input.

--- src/main/java/com/example/todoz/repos/TodoRepo.java
+++ src/main/java/com/example/todoz/repos/TodoRepo.java
@@ -1,8 +1,11 @@
 package com.example.todoz.repos;
 
+import com.example.todoz.models.Task;
 import com.example.todoz.models.Todo;
 import com.example.todoz.models.User;
 import org.springframework.data.repository.ListCrudRepository;
+
+import java.util.List;
 
 public interface TodoRepo extends ListCrudRepository<Todo, User> {
 }
--- src/main/java/com/example/todoz/services/TaskService.java
+++ src/main/java/com/example/todoz/services/TaskService.java
@@ -1,4 +1,32 @@
 package com.example.todoz.services;
 
+import com.example.todoz.models.Task;
+import com.example.todoz.repos.TaskRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
 public class TaskService {
+
+    private final TaskRepo taskRepo;
+
+    @Autowired
+    public TaskService(TaskRepo taskRepo) {
+        this.taskRepo = taskRepo;
+    }
+
+    public List<Task> filterByRating(Integer ratingNumber){
+        if(ratingNumber == 0 || ratingNumber > 4){
+            throw new RuntimeException("Input Integer must have value between 1 and 4.");
+        } else {
+            List<Task> allTasks = taskRepo.findAll();
+            List<Task> filteredTasks = allTasks.stream()
+                    .filter(t -> t.getPriority().equals(ratingNumber))
+                    .toList();
+            return filteredTasks;
+        }
+    }
 }
