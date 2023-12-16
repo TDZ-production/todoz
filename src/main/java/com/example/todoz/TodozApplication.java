ee45b3ae557f08d137037eb1eb001d0591c063ac
Tuan Do Van <do.van.tuan@seznam.cz>
2023-12-16 20:23:28 +0100

    custom login page connected with security

--- src/main/java/com/example/todoz/TodozApplication.java
+++ src/main/java/com/example/todoz/TodozApplication.java
@@ -69,7 +69,7 @@ public class TodozApplication implements CommandLineRunner {
         notificationService.save(new Notification(1L, "Can you already do it shithead!", null));
         notificationService.save(new Notification(1L, "Procrastinating huh...", null));
         notificationService.save(new Notification(1L, "Can you be less useless?", null));
-        notificationService.save(new Notification(0L, "The number of lasting days is same as the number of bitches you have... ZERO!", null));
+        notificationService.save(new Notification(0L, "The number of remaining days is same as the number of bitches you have... ZERO!", null));
         notificationService.save(new Notification(0L, "Today is the day bitch!", null));
         notificationService.save(new Notification(0L, "Can you like... DO IT?!", null));
         notificationService.save(new Notification(null, "If you dont want to set the Deadline you better do it now!", null));
--- src/main/java/com/example/todoz/security/SecurityConfig.java
+++ src/main/java/com/example/todoz/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.todoz.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -9,10 +10,22 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(request -> request.requestMatchers("/login")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated())
+                .formLogin(form -> form.loginPage("/login").permitAll());
+
+        return http.build();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
