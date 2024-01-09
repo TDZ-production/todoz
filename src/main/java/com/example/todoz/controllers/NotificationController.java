02d8047582b0533fbfc210f4ae50beee4b09dc14
Sime <bertoluchi2003@gmail.com>
2024-01-09 11:56:13 +0100

    service worker works but it makes many subscription for individual user

--- src/main/java/com/example/todoz/controllers/NotificationController.java
+++ src/main/java/com/example/todoz/controllers/NotificationController.java
@@ -2,9 +2,12 @@ package com.example.todoz.controllers;
 
 import com.example.todoz.services.MessageService;
 import nl.martijndwars.webpush.Subscription;
+import org.springframework.boot.SpringApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+
+import java.security.Principal;
 
 @Controller
 public class NotificationController {
@@ -16,10 +19,10 @@ public class NotificationController {
     }
 
     @PostMapping("/send-notification")
-    public String sendNotification(@RequestBody Subscription subscription) {
-        System.out.println("New subscription: " + subscription);
-        messageService.subscribe(subscription);
-        messageService.sendNotifications();
+    public String sendNotification(@RequestBody Subscription subscription, Principal principal) {
+        System.out.println("New subscription: " + subscription.keys.auth);
+        messageService.subscribe(subscription, principal);
+        messageService.sendNotifications(principal);
         return "redirect:/";
     }
 
