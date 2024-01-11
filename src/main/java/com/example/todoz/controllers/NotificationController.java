40cbd6653e9e64776bb92535ff0688995aeb2c34
Sime <bertoluchi2003@gmail.com>
2024-01-11 15:54:34 +0100

    NotificationController fixed

--- src/main/java/com/example/todoz/controllers/NotificationController.java
+++ src/main/java/com/example/todoz/controllers/NotificationController.java
@@ -1,36 +1,41 @@
-//package com.example.todoz.controllers;
-//
-//import com.example.todoz.services.MessageService;
-//import nl.martijndwars.webpush.Subscription;
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//
-//import java.security.Principal;
-//
-//@Controller
-//public class NotificationController {
-//
-//    private final MessageService messageService;
-//
-//    public NotificationController(MessageService messageService) {
-//        this.messageService = messageService;
-//    }
-//
-//    @PostMapping("/subscribe")
-//    public String subscribe(@RequestBody Subscription subscription, Principal principal) {
-//        System.out.println("New subscription: " + subscription.keys.auth);
-//        messageService.subscribe(subscription, user);
-//        messageService.sendNotifications(principal, subscription);
-//        return "redirect:/";
-//    }
-//
-//    @GetMapping("/send-notification")
-//    public String sendNotification(Subscription subscription, Principal principal) {
-//        messageService.sendNotifications(principal, subscription);
-//        return "redirect:/";
-//    }
-//
-//}
+package com.example.todoz.controllers;
+
+import com.example.todoz.models.User;
+import com.example.todoz.services.MessageService;
+import com.example.todoz.services.UserService;
+import nl.martijndwars.webpush.Subscription;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.security.Principal;
+
+@Controller
+public class NotificationController {
+
+    private final MessageService messageService;
+    private final UserService userService;
+
+    public NotificationController(MessageService messageService, UserService userService) {
+        this.messageService = messageService;
+        this.userService = userService;
+    }
+
+    @PostMapping("/subscribe")
+    public String subscribe(@RequestBody Subscription subscription, Principal principal) {
+        messageService.subscribe(subscription, getUser(principal));
+        System.out.println("New subscription: " + subscription.keys.auth);
+        return "redirect:/";
+    }
+
+    @GetMapping("/send-notification")
+    public String sendNotification() {
+        messageService.sendNotifications();
+        return "redirect:/";
+    }
+    private User getUser(Principal principal) {
+        return userService.findByUsername(principal.getName()).orElseThrow(RuntimeException::new);
+    }
+
+}
