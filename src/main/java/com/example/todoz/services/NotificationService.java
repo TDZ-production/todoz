f43a74515de89c1420dccbdc004e5bba108f5b08
Sime <bertoluchi2003@gmail.com>
2024-01-15 17:44:45 +0100

    notification functions

--- src/main/java/com/example/todoz/services/NotificationService.java
+++ src/main/java/com/example/todoz/services/NotificationService.java
@@ -1,14 +1,38 @@
 package com.example.todoz.services;
 
+import com.example.todoz.dtos.NotificationDTO;
+import com.example.todoz.models.Task;
 import com.example.todoz.repos.NotificationRepo;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class NotificationService {
 
     private final NotificationRepo notificationRepo;
+    private final TaskService taskService;
 
-    public NotificationService(NotificationRepo notificationRepo) {
+
+    public NotificationService(NotificationRepo notificationRepo, TaskService taskService) {
         this.notificationRepo = notificationRepo;
+        this.taskService = taskService;
     }
+
+//    public List<NotificationDTO> getMorningNotifications(){
+//        List<Task> task = getTaskForToday();
+//
+//    }
+//    public List<Task> getTaskForToday(){
+//
+//    }
+
+//    public List<Task> getMoreTasks(){
+//
+//    }
+//    public List<NotificationDTO> putTaskOnNotification(){
+//
+//    }
+
+
 }
