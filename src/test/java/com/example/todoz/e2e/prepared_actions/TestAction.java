706b6713516f453280b316ba9a45b1f72caf6337
BelovUser <139882857+BelovUser@users.noreply.github.com>
2024-01-15 15:19:05 +0100

    Create new method

--- src/test/java/com/example/todoz/e2e/prepared_actions/TestAction.java
+++ src/test/java/com/example/todoz/e2e/prepared_actions/TestAction.java
@@ -3,6 +3,8 @@ package com.example.todoz.e2e.prepared_actions;
 import com.microsoft.playwright.Page;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
+
 @Component
 public class TestAction {
     public void RegisterAndLogin(Page page, String webUrl) {
@@ -37,6 +39,18 @@ public class TestAction {
         page.evaluate("setPriority(" + priority + ");");
         page.click("#calendar_icon");
         page.type("#date",date);
+    }
+
+    public void createTasksForAllCases(Page page, LocalDate date){
+        LocalDate tomorrow = date.plusDays(1);
+        LocalDate nextWeek = date.plusWeeks(1);
+        LocalDate nextTwoWeeks = date.plusWeeks(2);
+
+        createDueDateTask(page,tomorrow.toString(),4);
+        createDueDateTask(page,nextWeek.toString(),4);
+        createDueDateTask(page,nextTwoWeeks.toString(),4);
+
+        createFourNoDueDateTasks(page);
     }
 
 }
