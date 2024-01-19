5dc3962cd07e7d14c201eee8f1b111dc34f257d9
BelovUser <139882857+BelovUser@users.noreply.github.com>
2024-01-19 17:01:39 +0100

    WIP

--- src/test/java/com/example/todoz/services/WeekReviewTesting.java
+++ src/test/java/com/example/todoz/services/WeekReviewTesting.java
@@ -1,25 +1,20 @@
 package com.example.todoz.services;
 
-import com.example.todoz.controllers.MainController;
-import com.example.todoz.models.DateManager;
 import com.example.todoz.models.Task;
 import com.example.todoz.models.User;
-import com.example.todoz.services.TaskService;
-import com.example.todoz.services.UserService;
+import com.example.todoz.models.Week;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.security.core.Authentication;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
+
 import java.security.Principal;
 import java.time.LocalDateTime;
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -31,41 +26,32 @@ import static org.mockito.Mockito.*;
 @AutoConfigureMockMvc
 public class WeekReviewTesting {
 
-    @Autowired
-    private MockMvc mockMvc;
 
-    @Mock
-    private DateManager dateManager;
-
-    @Mock
+    private final MockMvc mockMvc;
+    @MockBean
     private WeekService weekService;
-
-    @Mock
+    @MockBean
     private TaskService taskService;
-
-    @InjectMocks
-    private MainController mainController;
+    @Autowired
+    public WeekReviewTesting(MockMvc mockMvc) {
+        this.mockMvc = mockMvc;
+    }
 
     @Test
+    @WithMockUser(username = "TestUser")
     public void WeekReview_CanAddTask_True() throws Exception {
         // arrange
-        User user = new User();
-        user.setUsername("TestUser");
-        user.setPassword("TestPassword");
-
         Task task = new Task();
         task.setDescription("TestTask");
         task.setDueDate(LocalDateTime.now().plusWeeks(1));
 
         when(taskService.findUpcomingTasks(any(User.class), anyInt(), anyInt()))
                 .thenReturn(List.of(task));
-        when(weekService.getCurrentWeek(any(User.class)))
-                .thenReturn(Optional.of(null));
         when(weekService.findPreviousWeek(any(User.class)))
-                .thenReturn(List.of(task));
+                .thenReturn(Optional.of(new Week()));
+        when(weekService.findCurrentWeek(any(User.class)))
+                .thenReturn(Optional.empty());
 
-        Authentication authentication = mock(Authentication.class);
-        when(authentication.getName()).thenReturn("TestUser");
         Principal principal = mock(Principal.class);
         when(principal.getName()).thenReturn("TestUser");
 
@@ -74,5 +60,6 @@ public class WeekReviewTesting {
                 .andExpect(status().isOk())
                 .andExpect(view().name("weekReview"))
                 .andExpect(model().attributeExists("user", "previousWeek", "upcomingTasks"));
+
     }
 }
