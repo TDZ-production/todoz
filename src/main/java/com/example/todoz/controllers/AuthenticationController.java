a9a96930ddec3bf5e33cb4ceac47e89a4a9e1165
Ales Novotny <hi@alesnovotny.cz>
2024-01-22 13:15:20 +0100

    release 0.2 (#76)
    * squashed
    
    * validation allows spaces
    
    * reverted try catch implementation, set up global const, better validation
    
    * fix&tune behaviour of inputs and buttons
    
    * revert last try-catch implementation
    
    * reset password styles update
    
    * userId fix
    
    * revert marge mistake (bg color)
    
    * tlacitka jsou dole
    
    * css fix and weekReview copy update
    
    * new stickman
    
    * refactor handling of WR submit text + copy update
    
    * WeekQuality! + cat in WR
    
    * copy update wr
    
    * new yellow priority color
    
    * no tasks info tuzing
    
    ---------
    
    Co-authored-by: Tuan Do Van <do.van.tuan@seznam.cz>
    Co-authored-by: Jan Vaníček <ye@email.cz>

--- src/main/java/com/example/todoz/controllers/AuthenticationController.java
+++ src/main/java/com/example/todoz/controllers/AuthenticationController.java
@@ -1,16 +1,23 @@
 package com.example.todoz.controllers;
 
 import com.example.todoz.dtos.RegisterDTO;
+import com.example.todoz.prtoken.PasswordResetToken;
+import com.example.todoz.services.EmailService;
 import com.example.todoz.models.User;
+import com.example.todoz.prtoken.PRTService;
 import com.example.todoz.services.UserService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.Optional;
+
+import static com.example.todoz.models.User.MINIMAL_PASSWORD_LENGTH;
 
 @Controller
 @RequiredArgsConstructor
@@ -18,6 +25,8 @@ public class AuthenticationController {
 
     private final UserService userService;
     private final PasswordEncoder passwordEncoder;
+    private final PRTService pRTService;
+    private final EmailService emailService;
 
     @GetMapping("/login")
     public String showLogin() {
@@ -29,32 +38,94 @@ public class AuthenticationController {
         return "redirect:/";
     }
 
-    @GetMapping("/register")
-    public String showRegisterPage() {
-        return "registerPage";
-    }
-
     @PostMapping("/logout")
     public String logout() {
         return "redirect:/login";
     }
 
+    @GetMapping("/register")
+    public String showRegisterPage() {
+        return "registerPage";
+    }
+
     @PostMapping("/register")
     public String register(RegisterDTO registerDTO, RedirectAttributes ra) {
-        Optional<User> temp = userService.findByUsername(registerDTO.username());
+        Optional<User> optUSer = userService.findByUsername(registerDTO.username());
 
-        if(temp.isPresent()) {
+        if (optUSer.isPresent()) {
             ra.addFlashAttribute("userExists", true);
+
             return "redirect:/register";
+        } else {
+            userService.createAndSave(registerDTO.username(), passwordEncoder.encode(registerDTO.password()), registerDTO.pussyMeter());
+
+            return "redirect:/login";
         }
-        else {
-            User user = new User();
-            user.setUsername(registerDTO.username());
-            user.setPassword(passwordEncoder.encode(registerDTO.password()));
-            user.setPussyMeter(registerDTO.pussyMeter());
-            userService.save(user);
+    }
+
+    @GetMapping("/resetPassword")
+    public String showForgotPassword() {
+        return "resetPassword";
+    }
+
+    @PostMapping("/resetPassword")
+    public String resetPasswordRequest(String email, final HttpServletRequest request, RedirectAttributes ra) {
+        Optional<User> optUser = userService.findByUsername(email);
+
+        if (optUser.isEmpty()) {
+            ra.addFlashAttribute("userNotFound", true);
+
+            return "redirect:/resetPassword";
         }
+        User user = optUser.get();
+        emailService.sendEmail(user.getUsername(), passwordResetEmailLink(request, pRTService.createPRToken(user).getToken()));
+        ra.addFlashAttribute("sent", true);
+
+        return "redirect:/resetPassword";
+    }
+
+    @GetMapping("/validateToken")
+    public String showNewPassword(@RequestParam(required = false) String token, RedirectAttributes ra) {
+        Optional<PasswordResetToken> optToken = pRTService.findByToken(token);
+
+        if (token == null || token.isEmpty() || optToken.isEmpty()) {
+            ra.addFlashAttribute("tokenNotFound", true);
+            return "redirect:/resetPassword";
+        } else if (!pRTService.validateToken(optToken.get())) {
+            ra.addFlashAttribute("tokenExpired", true);
+            pRTService.deleteByToken(token);
+            return "redirect:/resetPassword";
+        } else {
+            ra.addFlashAttribute("userId", optToken.get().getUser().getId());
+            return "redirect:/newPassword";
+        }
+    }
+
+    @GetMapping("/newPassword")
+    public String showNewPassword() {
+        return "newPassword";
+    }
+
+    @PostMapping("/newPassword")
+    public String resetPassword(String password, String confirmation, RedirectAttributes ra, Long userId) {
+        if (!password.equals(confirmation)) {
+            ra.addFlashAttribute("doesNotMatch", true);
+            ra.addFlashAttribute("userId", userId);
+            return "redirect:/newPassword";
+        } else if (password.length() < MINIMAL_PASSWORD_LENGTH) {
+            ra.addFlashAttribute("invalid", true);
+            ra.addFlashAttribute("userId", userId);
+            return "redirect:/newPassword";
+        }
+
+        pRTService.deleteAllByUserId(userId);
+        userService.updatePassword(userId, passwordEncoder.encode(password));
+        ra.addFlashAttribute("passwordReset", true);
 
         return "redirect:/login";
+    }
+
+    private String passwordResetEmailLink(HttpServletRequest request, String token) {
+        return "https://" + request.getServerName() + request.getContextPath() + "/validateToken?token=" + token;
     }
 }
