309fa4cd5e64cd6f8c4e1a1ac96f18da6fc309b5
BelovUser <139882857+BelovUser@users.noreply.github.com>
2024-01-25 11:39:14 +0100

    Made a random generated username

--- src/test/java/com/example/todoz/e2e/prepared_actions/TestAction.java
+++ src/test/java/com/example/todoz/e2e/prepared_actions/TestAction.java
@@ -3,24 +3,27 @@ package com.example.todoz.e2e.prepared_actions;
 import com.microsoft.playwright.Page;
 import org.springframework.stereotype.Component;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
+
+import java.util.Random;
 
 @Component
 public class TestAction {
 
     public void RegisterAndLogin(Page page, String webUrl) {
+
+        String username = String.format("testUser%s@mail.com", new Random().nextInt());
+
         //Register action
         page.navigate(webUrl + "/register");
 
-        page.type("input[name=username]", "testUser@mail.com");
+        page.type("input[name=username]", username);
         page.type("input[name=password]", "testPassword");
         page.click("button[type=submit]");
 
         page.waitForTimeout(2000);
 
         //Login action
-        page.type("input[name=username]", "testUser@mail.com");
+        page.type("input[name=username]", username);
         page.type("input[name=password]", "testPassword");
         page.click("button[type=submit]");
     }
