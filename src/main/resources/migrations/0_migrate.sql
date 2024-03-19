e1be0d7d66336b2d6ac504d22ccd5ea72b83d81b
ales <hi@alesnovotny.cz>
2024-03-19 12:21:02 +0100

    migration refactoring

---
+++ src/main/resources/migrations/0_migrate.sql
@@ -0,0 +1,7 @@
+alter table task drop column due_date_week_number;
+
+alter table task add column done_at DATETIME default null;
+update task set done_at = created_at WHERE done = true;
+alter table task drop column done;
+
+alter table task modify column left_behind DATETIME default null;
