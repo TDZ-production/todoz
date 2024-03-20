f26e20d230a14978e72d7da6e94557bf994129de
ales <hi@alesnovotny.cz>
2024-03-20 14:46:41 +0100

    + schema.sql and refactor migrations

--- src/main/resources/migrations/240201_first.sql
+++ src/main/resources/migrations/240201_first.sql
@@ -3,3 +3,4 @@ alter table task add column done_at DATETIME default null;
 update task set done_at = created_at WHERE done = true;
 alter table task drop column done;
 alter table task modify column left_behind DATETIME default null;
+alter table users add column created_at DATETIME default now();
